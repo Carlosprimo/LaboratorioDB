6713bd29a422ecefe06311d20aab4aaaeafc67b7
CarlosPrimo <carlosprimob@gmail.com>
2020-10-07 11:45:18 -0500

    Change api.js

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,12 @@
 var express = require('express');
 var router = express.Router();
 
+// Models
+var Product = require('../models/product');
+
 // Routes
-router.get('/products', function(req, res) {
-   res.send('api works!');
-});
+Product.methods(['get', 'put', 'post', 'delete']);
+Product.register(router, '/products');
 
 // Return router
 module.exports = router;
