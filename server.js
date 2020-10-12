b98c48a14b373995371aabc2e7a1a80f2279b95c
CarlosPrimo <carlosprimob@gmail.com>
2020-10-12 14:03:58 -0500

    Deleted index.html and Improvements in documentation and readings

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // MongoDB
 mongoose.connect('mongodb+srv://pruebaDB:1qwer5432@cluster0.uakml.mongodb.net/pruebaDB?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
@@ -11,9 +11,15 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Settings
+
+app.set('port',process.env.PORT || 3000);
+
 // Routes
 app.use('/api', require('./routes/api'));
 
 // Start server
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(app.get('port'),()=>{
+    console.log('Listening on port 3000');
+});
+
